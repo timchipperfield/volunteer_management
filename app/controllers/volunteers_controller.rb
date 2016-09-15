7a39516c2938ef94b586c58bf9824257fa76bbb1
Tim <timchipperfield@gmail.com>
2016-09-15 18:35:02 +0100

    user can add additional volunteers

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -5,4 +5,18 @@ class VolunteersController < ApplicationController
     @volunteers = Volunteer.all
   end
 
+  def new
+  end
+
+  def create
+    Volunteer.create(volunteer_params)
+    redirect_to '/volunteers'
+  end
+
+  private
+
+  def volunteer_params
+    params.require(:volunteer).permit(:name, :email, :tel)
+  end
+
 end
--- spec/features/volunteer_spec.rb
+++ spec/features/volunteer_spec.rb
@@ -6,9 +6,19 @@ feature 'volunteers page' do
       visit '/volunteers'
       expect(page).to have_content('There are no volunteers yet!')
     end
+
+    scenario 'create new user' do
+      visit '/volunteers'
+      click_link('Add Volunteer')
+      fill_in('Name', :with => 'James Brown' )
+      fill_in('Tel', :with => 1234 )
+      fill_in('Email', :with => 'james@brown.com' )
+      click_button('Create Volunteer')
+      expect(page).to have_content('James Brown')
+    end
   end
 
-  context 'volunteer is added withcontact details' do
+  context 'volunteer exists withcontact details' do
     before do
       Volunteer.create(name: 'James Bond')
     end
